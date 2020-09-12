0dc3d63fc7600fc0d9ef8648a903347038a6f775
chonhnm <chonhnm@gmail.com>
2020-09-12 18:11:23 +0800

    dp topdown

--- src/_10_reg_exp_match/RegExpMatch.c
+++ src/_10_reg_exp_match/RegExpMatch.c
@@ -2,10 +2,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 bool isMatch(char *, char *);
 bool isMatch2(char *, char *);
-bool isMatchDp2(char *, char *);
+bool isMatchDpBottomUp(char *, char *);
+bool isMatchDpTopDown(char *, char *);
 
 int main() {
     assert(false == isMatch("aa", "a"));
@@ -26,14 +28,23 @@ int main() {
     assert(false == isMatch2("a", ".*..a*"));
     assert(true == isMatch2("", "c*c*"));
 
-    assert(false == isMatchDp2("aa", "a"));
-    assert(true == isMatchDp2("aa", "a*"));
-    assert(true == isMatchDp2("ab", ".*"));
-    assert(true == isMatchDp2("aab", "c*a*b"));
-    assert(false == isMatchDp2("mississippi", "mis*is*p*."));
-    assert(true == isMatchDp2("aaa", "a*a"));
-    assert(false == isMatchDp2("a", ".*..a*"));
-    assert(true == isMatchDp2("", "c*c*"));
+    assert(false == isMatchDpBottomUp("aa", "a"));
+    assert(true == isMatchDpBottomUp("aa", "a*"));
+    assert(true == isMatchDpBottomUp("ab", ".*"));
+    assert(true == isMatchDpBottomUp("aab", "c*a*b"));
+    assert(false == isMatchDpBottomUp("mississippi", "mis*is*p*."));
+    assert(true == isMatchDpBottomUp("aaa", "a*a"));
+    assert(false == isMatchDpBottomUp("a", ".*..a*"));
+    assert(true == isMatchDpBottomUp("", "c*c*"));
+
+    assert(false == isMatchDpTopDown("aa", "a"));
+    assert(true == isMatchDpTopDown("aa", "a*"));
+    assert(true == isMatchDpTopDown("ab", ".*"));
+    assert(true == isMatchDpTopDown("aab", "c*a*b"));
+    assert(false == isMatchDpTopDown("mississippi", "mis*is*p*."));
+    assert(true == isMatchDpTopDown("aaa", "a*a"));
+    assert(false == isMatchDpTopDown("a", ".*..a*"));
+    assert(true == isMatchDpTopDown("", "c*c*"));
 }
 
 bool isMatch(char *s, char *p) {
@@ -78,15 +89,11 @@ bool isMatch2(char *s, char *p) {
     }
 }
 
-bool isMatchDp2(char *s, char *p) {
+bool isMatchDpBottomUp(char *s, char *p) {
     size_t slen = strlen(s);
     size_t plen = strlen(p);
     bool dp[slen + 1][plen + 1];
-    for (size_t i = 0; i <= slen; i++) {
-        for (size_t j = 0; j <= plen; j++) {
-            dp[i][j] = false;
-        }
-    }
+    memset(dp, false, (slen + 1) * (plen + 1));
     dp[slen][plen] = true;
 
     for (int i = slen; i >= 0; i--) {
@@ -102,6 +109,39 @@ bool isMatchDp2(char *s, char *p) {
     return dp[0][0];
 }
 
-bool isMatchDp(char *s, char *p) {
-    
+bool isMatchDpTopDown(char *s, char *p) {
+    bool helper(int i, int j, char *s, char *p, bool* []);
+    size_t slen = strlen(s);
+    size_t plen = strlen(p);
+    bool *dp[slen + 1];
+    for (size_t i = 0; i <= slen ; i ++) {
+        dp[i] = calloc(sizeof *dp[i], plen + 1);
+    }
+    bool ans = helper(0, 0, s, p, dp);
+    for (size_t i = 0; i <= slen ; i ++) {
+        free(dp[i]);
+    }
+    return ans;
+}
+
+bool helper(int i, int j, char *s, char *p, bool *dp[]) {
+    size_t plen = strlen(p);
+    // if (dp[i][j] != 255) {
+    //     return dp[i][j];
+    // }
+    size_t slen = strlen(s);
+    bool ans;
+    if (j == plen) {
+        ans = i == slen;
+    } else {
+        bool firstMatch = i < slen && (*(s + i) == *(p + j) || *(p + j) == '.');
+        if (j + 1 < plen && *(p + j + 1) == '*') {
+            ans = helper(i, j + 2, s, p, dp) ||
+                  (firstMatch && helper(i + 1, j, s, p, dp));
+        } else {
+            ans = firstMatch && helper(i + 1, j + 1, s, p, dp);
+        }
+    }
+    dp[i][j] = ans;
+    return ans;
 }
